36b3d74401f251bfc947b7f1228e6c40379eadf2
Casandra Calderon <angie200896@gmail.com>
2021-12-09 20:47:58 -0400

    Opciones para agregar, eliminar y editar avatar agregado

--- src/pages/PresentCard/PresentCard.js
+++ src/pages/PresentCard/PresentCard.js
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './PresentCard.css';
-import avatar from '../../img/programador.png'
-import avatar1 from '../../img/gerente.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser} from "@fortawesome/free-solid-svg-icons";
 import Cookies from "universal-cookie";
+import axios from "axios";
 
+const url = "http://localhost:8000/api/avatar"
 const cookies = new Cookies();
-const PresentCard = () => {
+
+class PresentCard extends Component {
+  state = {
+    data : []
+  }
+  componentDidMount() {
+    this.peticionGet();
+  }
+  peticionGet=()=>{
+    axios.get(url).then(response=>{
+      this.setState({data: response.data.filter(e => e.RU === cookies.get("RU"))});
+      console.log(this.state.data)
+    }).catch(error=>{
+      console.log(error.message);
+    })
+
+    }
+  render() {
     return (
       <div>
-        <br /><br />
+        <br /><br /><br /><br /><br />
           <div className="card col-md-6 offset-md-3 text-white bg-dark mb-3" id= 'card'>
             <div className="row no-gutters">
               <div className="col-md-2">
-                <img src={cookies.get("image") === "ADMINISTRADOR"? avatar : avatar1} className="card-img" alt="..."/>
+                <img src={this.state.data.length !== 0? `http://localhost:8000/${this.state.data[0]?.image}` : "http://localhost:8000/uploads/gerente.png" } className="card-img" alt="..."/>
               </div>
               <div className="col-md-8">
                 <div className="text-center" id="cardText">
                 <br /><br />
-                  <h3 className="card-title"><FontAwesomeIcon icon={faUser} /> {cookies.get("Nombre")} {cookies.get("Ap_Paterno")} {cookies.get("Ap_Materno")}</h3>
+                  <h4 className="card-title"><FontAwesomeIcon icon={faUser} /> {cookies.get("Nombre")} {cookies.get("Ap_Paterno")} {cookies.get("Ap_Materno")}</h4>
                   <h4 className="card-title">RU: {cookies.get("RU")}</h4>
                   <p className="cargo">{cookies.get("Cargo")}</p>
                 </div>
@@ -30,5 +47,7 @@ const PresentCard = () => {
         </div>
         
     )
+  }
+    
 }
 export default PresentCard;
--- src/pages/miPerfil/MiPerfil.js
+++ src/pages/miPerfil/MiPerfil.js
@@ -1,176 +1,247 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FaUserEdit, FaTrash } from "react-icons/fa";
 import Swal from "sweetalert2";
 import axios from "axios";
-import "./MiPerfil.css"
+import "./MiPerfil.css";
 import Cookies from "universal-cookie";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
-
 const cookies = new Cookies();
-const url = "http://localhost:8000/api/avatar"
-const usuario = `http://localhost:8000/api/${cookies.get("Cargo")}`
+const url = "http://localhost:8000/api/avatar";
+const usuario = `http://localhost:8000/api/${cookies.get("Cargo")}`;
 
 class MiPerfil extends Component {
-    state = {
-      modalInsertar: false,
-      modalEliminar: false,
-      tipoModal: "",
-      data: [],
-      user: [],
-      form: {
-        _id:"",
-        RU: "",
-        image: null
-    }
-    }
+  state = {
+    modalInsertar: false,
+    modalEliminar: false,
+    tipoModal: "",
+    prueba: false,
+    data: [],
+    user: [],
+    form: {
+      _id: "",
+      RU: "",
+      image: null,
+    },
+  };
 
-    componentDidMount() {
-      this.peticionGet();
-    }
+  componentDidMount() {
+    this.peticionGet();
+  }
 
-    peticionGet=()=>{
-      axios.get(usuario)
+  peticionGet = () => {
+    axios
+      .get(usuario)
       .then((response) => {
-        this.setState({user: response.data.filter(e => e.RU === cookies.get("RU"))});
-      })
-      .catch((error)=>{
-        console.log(error);
-      })
-      axios.get(url).then(response=>{
-        this.setState({data: response.data.filter(e => e.RU === cookies.get("RU"))});
-        console.log(this.state.data)
-      }).catch(error=>{
-        console.log(error.message);
-      })
-
-      }
-    
-      peticionPost=async()=>{
-        delete this.state.form?._id;
-        const fd = new FormData();
-        fd.append("image", this.state.form?.image)
-        fd.append("_id", this.state.form?._id,)
-        fd.append("RU", cookies.get("RU"))
-       await axios.post(`${url}/create`,
-        fd
-        ).then(response=>{
-          this.setState({modalInsertar: false});
-          this.peticionGet();
-        }).catch(error=>{
-          console.log(error.message);
-        })
-      }
-
-      peticionDelete=()=>{
-        axios.delete(`${url}/delete/${this.state.data[0]?._id}`).then(response=>{
-          this.peticionGet();
-        })
-      }
-
-      modalInsertar=()=>{
-        this.setState({modalInsertar: !this.state.modalInsertar});
-      }
-
-      seleccionarAvatar=(avatar)=>{
         this.setState({
-          tipoModal: 'actualizar',
-          form: {
-            _id: avatar._id,
-            RU: avatar.RU,
-            image: avatar.image,
-          }
-        })
-      }
-
-      modalEliminar = () => {
-        Swal.fire({
-          title: `¡Espera!`,
-          text: `¿Esta seguro de eliminar su foto de perfil?`,
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          confirmButtonText: 'Si'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            this.peticionDelete()
-            Swal.fire(
-              '¡Eliminado!',
-              'Su solicitud se ejecuto de manera exitosa',
-              'success'
-            )
-          } 
-        })
-      }
-
-      handleChange=async e=>{
-        e.persist();
-        await this.setState({
-          form:{
-            ...this.state.form,
-            image: e.target.files[0]
-          }
+          user: response.data.filter((e) => e.RU === cookies.get("RU")),
         });
-        }
-      
-        
-    
-    render (){
-      const { user  } = this.state;
-      return (
-        <div id="Perfil">
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(url)
+      .then((response) => {
+        this.setState({
+          data: response.data.filter((e) => e.RU === cookies.get("RU")),
+        });
+        console.log(this.state.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  peticionPost = async () => {
+    delete this.state.form?._id;
+    const fd = new FormData();
+    fd.append("image", this.state.form?.image);
+    fd.append("_id", this.state.form?._id);
+    fd.append("RU", cookies.get("RU"));
+    await axios
+      .post(`${url}/create`, fd)
+      .then((response) => {
+        this.setState({ modalInsertar: false });
+        this.peticionGet();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  peticionDelete = () => {
+    axios
+      .delete(`${url}/delete/${this.state.data[0]?._id}`)
+      .then((response) => {
+        this.peticionGet();
+      });
+  };
+
+  modalInsertar = () => {
+    this.setState({ modalInsertar: !this.state.modalInsertar });
+  };
+
+  seleccionarAvatar = (avatar) => {
+    this.setState({
+      tipoModal: "actualizar",
+      form: {
+        _id: avatar._id,
+        RU: avatar.RU,
+        image: avatar.image,
+      },
+    });
+  };
+
+  modalEliminar = () => {
+    Swal.fire({
+      title: `¡Espera!`,
+      text: `¿Esta seguro de eliminar su foto de perfil?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.peticionDelete();
+        Swal.fire(
+          "¡Eliminado!",
+          "Su solicitud se ejecuto de manera exitosa",
+          "success"
+        );
+      }
+    });
+  };
+
+  AlertEditado = () => {
+    Swal.fire(
+      "Foto de perfil actualizada",
+      "Su solicitud se ejecuto de manera exitosa",
+      "success"
+    );
+  };
+
+  alertAgregado = () => {
+    Swal.fire(
+      "Foto de perfil agregada",
+      "Su solicitud se ejecuto de manera exitosa",
+      "success"
+    );
+  };
+
+  peticionPut = () => {
+    this.peticionDelete();
+    this.peticionPost();
+    this.AlertEditado();
+    this.modalInsertar();
+  };
+
+  actualizar = () => {
+    this.modalInsertar();
+  };
+
+  handleChange = async (e) => {
+    e.persist();
+    await this.setState({
+      form: {
+        ...this.state.form,
+        image: e.target.files[0],
+      },
+    });
+  };
+
+  render() {
+    const { user } = this.state;
+    return (
+      <div id="Perfil">
         <div className="contenedor">
           <section>
             <img
               className="photo"
-              src={this.state.data.length !== 0? `http://localhost:8000/${this.state.data[0]?.image}` : "http://localhost:8000/uploads/gerente.png" }
+              src={
+                this.state.data.length !== 0
+                  ? `http://localhost:8000/${this.state.data[0]?.image}`
+                  : "http://localhost:8000/uploads/gerente.png"
+              }
               alt=""
             />
-            
+
             <div className="options">
-              <button className="btn btn-dark" onClick={()=>{this.setState({form: null, tipoModal: "insertar"}); this.modalInsertar()}}><FaUserEdit/> Editar</button>{"  "}<button className="btn btn-dark" onClick={()=>this.modalEliminar()}><FaTrash/>Eliminar</button>
+              {this.state.data.length !== 0 ? (
+                <button
+                  className="btn btn-dark"
+                  onClick={() => {
+                    this.setState({ form: null, tipoModal: "actualizar" });
+                    this.actualizar();
+                  }}
+                >
+                  <FaUserEdit /> Editar
+                </button>
+              ) : (
+                <button
+                  className="btn btn-dark"
+                  onClick={() => {
+                    this.setState({ form: null, tipoModal: "insertar" });
+                    this.modalInsertar();
+                  }}
+                >
+                  <FaUserEdit />
+                  Agregar
+                </button>
+              )}{" "}
+              {this.state.data.length !== 0 ? (
+                <button
+                  className="btn btn-dark"
+                  onClick={() => this.modalEliminar()}
+                >
+                  <FaTrash />
+                  Eliminar
+                </button>
+              ) : (
+                console.log("prueba")
+              )}
             </div>
           </section>
           <section id="data">
-            <div id='user-input'>
+            <div id="user-input">
               <span>Nombre</span>
               <span>{user[0]?.Nombre}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Apellido Paterno</span>
               <span>{user[0]?.Ap_Paterno}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Apellido Materno</span>
               <span>{user[0]?.Ap_Materno}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>CI</span>
               <span>{user[0]?.CI}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>RU</span>
               <span>{user[0]?.RU}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Email</span>
               <span>{user[0]?.Email}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Telefono</span>
               <span>{user[0]?.Telefono}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Nombre de Usuario</span>
               <span>{user[0]?.username}</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Contraseña</span>
               <span>********</span>
             </div>
-            <div id='user-input'>
+            <div id="user-input">
               <span>Cargo</span>
               <span>{user[0]?.Cargo}</span>
             </div>
@@ -178,32 +249,54 @@ class MiPerfil extends Component {
         </div>
 
         <Modal centered isOpen={this.state.modalInsertar}>
-          <ModalHeader style={{display: 'block'}}>
-          <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
+          <ModalHeader style={{ display: "block" }}>
+            <span
+              style={{ float: "right" }}
+              onClick={() => this.modalInsertar()}
+            >
+              x
+            </span>
           </ModalHeader>
           <ModalBody>
             <div className="form-group">
-            <label>Seleccionar archivo...</label>
-            <input className="form-control" type="file" name= "image" onChange={this.handleChange} />
+              <label>Seleccionar archivo...</label>
+              <input
+                className="form-control"
+                type="file"
+                name="image"
+                onChange={this.handleChange}
+              />
             </div>
           </ModalBody>
           <ModalFooter>
-          {this.state.tipoModal==='insertar'?
-                        <button className="btn btn-dark" onClick={()=>this.peticionPost()}>
-                        Insertar
-                      </button>: <button className="btn btn-success" onClick={()=>this.peticionPut()}>
-                        Actualizar
-                      </button>
-                        }
-                        <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+            {this.state.tipoModal === "insertar" ? (
+              <button
+                className="btn btn-dark"
+                onClick={() => {
+                  this.peticionPost();
+                  this.alertAgregado();
+                }}
+              >
+                Insertar
+              </button>
+            ) : (
+              <button
+                className="btn btn-success"
+                onClick={() => this.peticionPut()}
+              >
+                Actualizar
+              </button>
+            )}
+            <button
+              className="btn btn-danger"
+              onClick={() => this.modalInsertar()}
+            >
+              Cancelar
+            </button>
           </ModalFooter>
-
         </Modal>
       </div>
-
-      
-    )
-
-    }
+    );
+  }
 }
 export default MiPerfil;
